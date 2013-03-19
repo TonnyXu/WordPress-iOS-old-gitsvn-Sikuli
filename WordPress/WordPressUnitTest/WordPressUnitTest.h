43a337eb9f034e74f31c9a2217f08937d6637509
Tonny Xu <tonny.xu@gmail.com>
2013-03-19 16:09:45 +0900

    Added new test target WordPressUnitTest

---
+++ WordPress/WordPressUnitTest/WordPressUnitTest.h
@@ -0,0 +1,13 @@
+//
+//  WordPressUnitTest.h
+//  WordPressUnitTest
+//
+//  Created by Tonny Xu on 3/19/13.
+//  Copyright (c) 2013 WordPress. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WordPressUnitTest : SenTestCase
+
+@end
---
+++ WordPress/WordPressUnitTest/WordPressUnitTest.m
@@ -0,0 +1,44 @@
+//
+//  WordPressUnitTest.m
+//  WordPressUnitTest
+//
+//  Created by Tonny Xu on 3/19/13.
+//  Copyright (c) 2013 WordPress. All rights reserved.
+//
+
+#import "WordPressUnitTest.h"
+
+@implementation WordPressUnitTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+  STAssertTrue(YES, @"OK");
+}
+- (void)testExample2
+{
+  STAssertTrue(YES, @"OK");
+}
+- (void)testExample3
+{
+  STAssertTrue(YES, @"OK");
+}
+- (void)testExample4
+{
+  STAssertTrue(YES, @"OK");
+}
+
+@end
